63e64ad9c8f9d1871d34c889f47cee46df4f60e6
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-02 16:46:01 +0200

    add commp MovieList

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDataAsync } from 'redux/dataMovie/dataThunks';
+import {
+  selectMovies,
+  selectStatus,
+  selectError,
+} from 'redux/dataMovie/dataSelectors';
+
+export const MovieList = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector(selectMovies);
+  const status = useSelector(selectStatus);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchDataAsync({ url: '/movie/popular' }));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Movie List</h1>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MovieList
