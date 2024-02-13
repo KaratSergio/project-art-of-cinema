60c4c571247245cb769313dea009b5106093fecb
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-13 17:51:18 +0200

    Update movieSelectors.js

--- src/redux/dataMovie/movieSelectors.js
+++ src/redux/dataMovie/movieSelectors.js
@@ -1,5 +1,5 @@
 export const selectError = state => state.error;
 export const selectStatus = state => state.status;
 export const selectMovies = state => state.movies.movies;
-export const selectMovieReviews = state => state.movies.movieReviews;
+export const selectMovieReviews = state => state.movies.movieReviews || [];
 export const selectMovieCredits = state => state.movies.movieCredits?.cast || [];
