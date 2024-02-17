6f66cc6e886f012152b0ead525704bf6c8b90df9
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-17 14:53:23 +0200

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,10 +13,32 @@ export const Footer = () => {
         <p>Millions of movies, TV shows and people to discover. Explore now.</p>
         <p>Made based on The Movie Data Base</p>
         <div className={scss.byApi}>
-          <p>
-            © 2024 KaratSergio. <span className={scss.tmdb}>TMDB</span>
-          </p>
-          <div className={scss.tmdbAccent}></div>
+          <p>© 2024 KaratSergio.</p>
+          <a
+            className={scss.tmdb}
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TMDB
+            <div className={scss.tmdbAccent}></div>
+          </a>
+        </div>
+      </div>
+      <div className={scss.footerNavBox}>
+        <div className={scss.NavBox}>
+          <h2>Movies</h2>
+          <p>popular</p>
+          <p>top rated</p>
+          <p>upcoming</p>
+          <p>now playing</p>
+        </div>
+        <div>
+          <h2>Series</h2>
+          <p>popular</p>
+          <p>top rated</p>
+          <p>upcoming</p>
+          <p>now playing</p>
         </div>
       </div>
     </footer>
