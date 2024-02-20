ab747e8609bc11e4e5e3cf7d8021fe79878567b5
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-20 10:58:18 +0200

    resp pad list

--- src/pages/Series/Series.jsx
+++ src/pages/Series/Series.jsx
@@ -1,16 +1,17 @@
 import { Footer } from '../../components/Footer/Footer';
-import { SeriesList } from '../../components/Series/SeriesList/SeriesList'
-// import scss from './Series.module.scss';
+import { SeriesList } from '../../components/Series/SeriesList/SeriesList';
+
+import scss from './Series.module.scss';
 
 export const Series = () => {
-    return (
-        <section>
-            <div>
-                <SeriesList />
-            </div>
-            <Footer />
-        </section>
-    );
+  return (
+    <section className={scss.container}>
+      <div>
+        <SeriesList />
+      </div>
+      <Footer />
+    </section>
+  );
 };
 
 export default Series;
