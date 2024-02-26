d3f10028c389f01c976ae9a3cd55486834bc1a6e
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-02-26 16:57:49 +0200

    responsive home\poster

--- src/components/MediaRandomizer/MediaRandomizer.jsx
+++ src/components/MediaRandomizer/MediaRandomizer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { selectMovies } from '../../redux/dataMovie/movieSelectors';
 import { fetchMoviesAsync } from '../../redux/dataMovie/movieThunks';
 
