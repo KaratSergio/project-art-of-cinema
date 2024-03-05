48bbdfcdb44f14497c1f9856970500b2a086d014
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-05 08:32:43 +0200

    ratingBar ver 2.0

--- src/components/RatingBar/RatingBar.jsx
+++ src/components/RatingBar/RatingBar.jsx
@@ -2,15 +2,32 @@ import React from 'react';
 import scss from './RatingBar.module.scss';
 
 export const RatingBar = ({ rating }) => {
-  console.log(rating);
   const maxRating = 10;
-  const barWidth = (rating / maxRating) * 100;
+  const circleRadius = 20;
+  const circumference = 2 * Math.PI * circleRadius;
+  const offset = circumference - (rating / maxRating) * circumference;
 
   return (
     <div className={scss.ratingBar}>
-      <div className={scss.ratingFill} style={{ width: `${barWidth}%` }}>
-        {rating} / 10
-      </div>
+      <svg className={scss.ratingCircle}>
+        <circle
+          className={scss.ratingCircle__background}
+          cx="24"
+          cy="24"
+          r={circleRadius}
+        ></circle>
+        <circle
+          className={scss.ratingCircle__fill}
+          cx="24"
+          cy="24"
+          r={circleRadius}
+          style={{
+            strokeDasharray: circumference,
+            strokeDashoffset: offset,
+          }}
+        ></circle>
+      </svg>
+      <div className={scss.ratingText}>{rating}</div>
     </div>
   );
 };
