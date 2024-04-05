ad926744bca135c7ce7990e4aa50cb60ad0279f9
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-04-05 05:24:46 +0300

    clean code scss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/project-art-of-cinema">
